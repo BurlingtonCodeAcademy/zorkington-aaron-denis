033b3e3d9f83cec445910799d40b8be9c5d873c3
Aaron Lavery <aaroncl55@gmail.com>
2019-06-18 09:39:45 -0400

    blah

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
+console.log ('182 Main St. You are standing on Main Street between Church and South Winooski. There is a door here. A keypad sits on the handle. On the door is a handwritten sign.')
 const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+//var roomNumber = ('roomOne', 'roomTwo', 'roomThree');
+const readlineInterface = readline.createInterface( 
+{
+  input : process.stdin,
+  output : process.stdout
+});
 
-function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, resolve);
-  });
-}
-
-// remember the StateMachine lecture
-// https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
-let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
+var rooms = {
+  'outside': {canChangeTo: ['foyer']},
+  'foyer': {canChangeTo: [ 'stairway']},
+  'stairway': {canChangeTo: ['hallway']},
+  'hallway': {canChangeTo: ['classroom']},
+  'classroom': {canChangeTo: ['hallway']},
+  'stairway': {canChangeTo: ['foyer']},
+  'foyer': {canChangeTo: ['outside']}
 };
 
-let currentState = "green";
+let currentState = "outside";
 
 function enterState(newState) {
-  let validTransitions = states[currentState].canChangeTo;
-  if (validTransitions.includes(newState)) {
+  let validTransitions = rooms[currentState].canChangeTo;
+  if (validTransitions.includes(room)) {
     currentState = newState;
   } else {
     throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
   }
 }
 
+readlineInterface.setPrompt('>_');
+readlineInterface.prompt();
+readlineInterface.on('line', function(answer)
+
+{
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
-  process.exit();
+
+  //const userPrompt = `\n>_`;
+ // let userAnswerLC = userAnswer.toLowerCase(); 
+  if (answer === 'gargle') {
+    console.log("Sorry, I don't know how to gargle.");
+  } 
+  else if (answer === 'read sign') {
+    console.log(
+      `The sign says "Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code 12345."`);
+      //return userPrompt  
+  } 
+    else if (answer == 'take sign') {
+      console.log ('Leave the sign alone, moron.')
+    }
+  else if (answer === 'enter code 12345')
+    {console.log('Success! The door opens. You enter the foyer and the door shuts behind you');
 }
+  
+  else { 
+    console.log("Sorry, I don't understand that.");
+    
+   // process.exit();
+  }
+}
+ readlineInterface.prompt();
+})
+
