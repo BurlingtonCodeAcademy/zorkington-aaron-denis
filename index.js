35f497cd63c418f63921380b3af70b9e5be32ad3
Denis Poirier <D.Poirier@me.com>
2019-06-18 10:26:04 -0400

    a few more changes - waiting for aaron pull and push

--- index.js
+++ index.js
@@ -13,24 +13,38 @@ function ask(questionText) {
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 
 let rooms = {     // was given in starter code 
-  'outside':   { canMoveTo: [ 'foyer' ],    roomMessage: [ `182 Main St.
+  'outside':   { 
+    canMoveTo: [ 'foyer' ],    
+    roomMessage: `182 Main St.
   You are standing on Main Street between Church and South Winooski.
   There is a door here. A keypad sits on the handle.
-  On the door is a handwritten sign. (If you wish to exit the game, at anytime, type
-    "exit")`], 
-    signMsg: [`The sign says "Welcome to Burlington Code Academy! Come on up to
-    the third floor. If the door is locked, use the code 12345."`], exitKey: [`12345`] },
-  'foyer':     { canMoveTo: [ `stairway` ], roomMessage: [`You are in foyer`],
-    signMsg: [`This sign says nothing.`],                           exitKey: [`exit1`] },
-  'stairway':  { canMoveTo: [ `hallway`],   roomMessage: [`You are in stairway`],
-    signMsg: [`This sign says nothing.`],                           exitKey: [`exit2`] },
-  'hallway':   { canMoveTo: [ 'classroom'], roomMessage: [`You are in hallway`],
-    signMsg: [`This sign says nothing.`],                           exitKey: [`exit3`] },
-  'classroom': { canMoveTo: [ 'hallway'],   roomMessage: [`You are in classroom!`],
-    signMsg: [`This sign says "classroom".`],                       exitKey: [`exit4`] }
+  On the door is a handwritten sign.`, 
+    signMsg: `The sign says "Welcome to Burlington Code Academy! Come on up to
+    the third floor. If the door is locked, use the code 12345."`, 
+    exitKey: `12345` },
+  'foyer':     {
+    canMoveTo: [ `stairway` ] ,
+    roomMessage: `You are in foyer`,
+    signMsg: `This sign says nothing.`,                           
+    exitKey: `exit1` },
+  'stairway':  {
+    canMoveTo: [ `hallway` ],   
+    roomMessage: `You are in stairway`,
+    signMsg: `This sign says nothing.`,                           
+    exitKey: `exit2` },
+  'hallway':   {
+    canMoveTo: [ 'classroom' ], 
+    roomMessage: `You are in hallway`,
+    signMsg: `This sign says nothing.`,                           
+    exitKey: `exit3` },
+  'classroom': {
+    canMoveTo: [ 'hallway' ],   
+    roomMessage: `You are in classroom!`,
+    signMsg: `This sign says "classroom".`,                       
+    exitKey: `exit4` }
 };
 
-let currentRoom = 'outside';
+let currentRoom = 'outside';  // state machine 
 function enterRoom(newRoom) {
   let validTransitions = rooms[currentRoom].canMoveTo;
   if (validTransitions.includes(newRoom)) {
@@ -39,23 +53,16 @@ function enterRoom(newRoom) {
     throw 'Invalid room transition attempted - from ' + currentRoom + ' to ' + newRoom;
   }
 }  
-  /*
-let currentRoom = null;  // given: example of saving rooms in global variables
-function moveToRoom(newRoom) {
-  if (canMoveToRoom(newRoom)) {
-    currentRoom = newRoom;
-  }
-}   */ 
 
 start();
 
 async function start() {
 
   const userPrompt = `\n>_`;
-  let userAnswer = await ask('outside'.roomMessage + userPrompt);
+  let userAnswer = await ask(rooms[currentRoom].roomMessage + userPrompt);
   let userAnswerLC = userAnswer.toLowerCase(); 
   while (userAnswerLC != 'exit') { 
-    if ((userAnswerLC == 'gargle') && (this.room == 'outside')) {
+    if ((userAnswerLC == 'gargle') && (room[currentRoom] == 'outside')) {
       console.log(`Sorry, I don't know how to ` + userAnswerLC + `.` + userPrompt);
     } else if (userAnswerLC == 'read sign') {
       console.log(signMsg + userPrompt);    
